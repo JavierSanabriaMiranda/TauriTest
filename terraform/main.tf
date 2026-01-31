61c41f7428084e00da2afafc5969ff59f5ae20ca
uo293758 <uo293758@uniovi.es>
2026-01-31 13:59:18 +0100

    feat: add GitHub Actions workflow for backend deployment and update PostgreSQL admin login

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   resource_group_name    = azurerm_resource_group.rg.name
   location               = azurerm_resource_group.rg.location
   version                = "13"
-  administrator_login    = "graphiteadmin"
+  administrator_login    = "testAdmin"
   administrator_password = var.db_password # Se pasa por variable secreta
   storage_mb             = 32768
   sku_name               = "B_Standard_B1ms"
